46c66f932cc03050754718cbc6abdc16f7c1c247
aayushtmG <aiyoustmg@gmail.com>
2025-11-25 17:30:11 +0545

    Update 2

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,46 +3,40 @@ package main
 import (
 	"errors"
 	"fmt"
-	// "html/template"
 	"net/http"
 	"strconv"
 
 	"github.com/aayushtmG/snippetbox/internal/models"
+	"github.com/aayushtmG/snippetbox/internal/validator"
+	"github.com/julienschmidt/httprouter"
 )
 
+type snippetCreateForm struct {
+	Title string
+	Content string
+	Expires int
+	validator.Validator
+}
+
+
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		app.notFound(w)
-		return
-	}
 	snippets, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w,err)
 		return
 	}
-
-	for _, snippet := range snippets {
-	fmt.Fprintf(w,"%+v\n",snippet)
-	}
 	
-	// files := []string{
-	// 	"./ui/html/base.tmpl",
-	// 	"./ui/html/pages/home.tmpl",
-	// 	"./ui/html/partials/nav.tmpl",
-	// }
-	// ts,err := template.ParseFiles(files...)
-	// if err!= nil {
-	// 	app.serverError(w,err)
-	// 	return	
-	// }
-	// err = ts.ExecuteTemplate(w,"base",nil)
-	// if err != nil {
-	// 	app.serverError(w,err)
-	// 	return	
-	// }
+	data := app.newTemplateData(r)
+	data.Snippets = snippets
+
+	app.render(w,http.StatusOK,"home.tmpl",data)
 }
+
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+
+	params := httprouter.ParamsFromContext(r.Context())
+
+	id, err := strconv.Atoi(params.ByName("id"))
 	if err != nil || id < 1 {
 		app.notFound(w)
 		return
@@ -58,26 +52,64 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 	}
+	data := app.newTemplateData(r)
+	data.Snippet = snippet
 
-	fmt.Fprintf(w,"%+v",snippet)
+
+	app.render(w,http.StatusOK,"view.tmpl",data)
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		w.Header().Set("Allow", http.MethodPost)
-		app.clientError(w,http.StatusMethodNotAllowed)
+	data := app.newTemplateData(r)
+	data.Form =  snippetCreateForm{
+		Expires: 365,
+	}
+
+	app.render(w,http.StatusOK,"create.tmpl",data)
+}
+
+func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+	
+	err := r.ParseForm()
+	if err != nil {
+		app.clientError(w,http.StatusBadRequest)
+		return 
+	}
+
+	//parsing and checking expires data
+	expires, err := strconv.Atoi(r.PostForm.Get("expires"))
+	if err != nil {
+		app.clientError(w,http.StatusBadRequest)
 		return
 	}
-	title := "0 snail"
-	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
-	expires := 7
 
-	id, err := app.snippets.Insert(title,content,expires)
+	form := snippetCreateForm{
+		Title: r.PostForm.Get("title"),
+		Content: r.PostForm.Get("content"),
+		Expires: expires,
+	}
+
+	//validating
+	form.CheckField(validator.NotBlank(form.Title),"title","This field cannot be blank")
+	form.CheckField(validator.MaxChars(form.Title,100),"title","This field cannot be more than 100 Characters long")
+	form.CheckField(validator.NotBlank(form.Content),"content","This field cannot be blank")
+	form.CheckField(validator.PermittedInt(form.Expires,1,7,365),"expires","This field must equal 1,7 or 365")
+	
+	if !form.Valid(){
+		data := app.newTemplateData(r)
+		data.Form = form
+		app.render(w,http.StatusUnprocessableEntity,"create.tmpl",data)
+		return
+	}
+
+
+
+	id, err := app.snippets.Insert(form.Title,form.Content,form.Expires)
 	if err != nil {
 		app.serverError(w,err)	
 		return
 	}
-	http.Redirect(w,r,fmt.Sprintf("/snippet/view?id=%d",id),http.StatusSeeOther)
+	http.Redirect(w,r,fmt.Sprintf("/snippet/view/%d",id),http.StatusSeeOther)
 }
 
 
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"database/sql"
 	"flag"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
+
 	"github.com/aayushtmG/snippetbox/internal/models"
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -15,6 +17,7 @@ type application struct {
 	errLog *log.Logger
 	infoLog *log.Logger
 	snippets *models.SnippetModel
+	templateCache map[string]*template.Template
 }
 
 func main() {
@@ -33,11 +36,16 @@ func main() {
 	}
 	defer db.Close()
 
-	
+	templateCache, err := newTemplateCache()	
+	if err != nil {
+		errLog.Fatal(err)
+	}
+
 	app := &application{
 		infoLog: infoLog,
 		errLog: errLog,
 		snippets: &models.SnippetModel{DB:db },
+		templateCache: templateCache,
 	}
 
 	srv := &http.Server{
